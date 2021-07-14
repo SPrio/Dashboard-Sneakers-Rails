f21633bc87931bdd2994c915079689040e39925e
SPrio <mahantasupriyo@gmail.com>
2021-07-14 22:12:24 +0530

    Add Post worker

---
+++ app/workers/posts_worker.rb
@@ -0,0 +1,12 @@
+class PostsWorker
+  include Sneakers::Worker
+
+  # this worker will connect to "dashboard.posts" queue
+  # env is set to nil since by default the actual name would be dashboard.posts_development
+  from_queue "dashboard.posts", env: nil
+
+  def work(raw_post)
+    RecentPosts.push(raw_post)
+    ack!
+  end
+end
